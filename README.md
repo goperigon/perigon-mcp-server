8e8678e66e5dd7ab88586c03d52130f4d19fc3e4
rekram1-node <aidenpcline@gmail.com>
2025-05-08 13:15:39 -0500

    chore: update readme, proj still in prog

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# perigon-mcp-server
-Official MCP server for the Perigon API 
+<p align="center">
+  <img src="https://goperigon.com/favicon.ico" width="120" alt="Perigon logo" />
+</p>
+
+<h1 align="center">Perigon&nbsp;MCP&nbsp;Server</h1>
+<p align="center">Official MCP Server for the <strong>Perigon&nbsp;API</strong></p>
+
+
+## Local Development
+
+We are using bun for package mgmt.
+
+```zsh
+bun i
+
+# Runs the mcp server and the mcp inspector
+bun start
+```
---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import { McpAgent } from "agents/mcp";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
+import { Hono } from "hono";
+import { Configuration, V1Api } from "@goperigon/perigon-ts";
+import { layout, homeContent } from "./utils";
+
+type Bindings = Env;
+
+const app = new Hono<{
+  Bindings: Bindings;
+}>();
+
+type Props = {
+  bearerToken: string;
+};
+
+type State = null;
+
+export class MyMCP extends McpAgent<Bindings, State, Props> {
+  server = new McpServer({
+    name: "Perigon News API",
+    version: "1.0.0",
+  });
+
+  async init() {
+    const perigon = new V1Api(
+      new Configuration({
+        apiKey: this.props.bearerToken,
+      }),
+    );
+
+    this.server.tool(
+      "Read articles",
+      {
+        q: z.string().describe("The query to search for articles."),
+      },
+      async ({ q }) => {
+        const result = await perigon.searchArticles({
+          q: q,
+        });
+        return {
+          content: [
+            {
+              type: "text",
+              text: JSON.stringify(result),
+            },
+          ],
+        };
+      },
+    );
+  }
+}
+
+app.get("/", async (c) => {
+  const content = await homeContent(c.req.raw);
+  return c.html(layout(content, "MCP Remote Auth Demo - Home"));
+});
+
+app.mount("/", async (req, env, ctx) => {
+  const authHeader = req.headers.get("authorization")?.toLowerCase();
+  if (!authHeader || !authHeader.startsWith("bearer ")) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const token = authHeader.replace("bearer ", "");
+  if (!token) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  ctx.props = {
+    bearerToken: token,
+  };
+
+  try {
+    const response = await MyMCP.mount("/sse").fetch(req, env, ctx);
+    return response ?? new Response("No Results", { status: 200 });
+  } catch (error) {
+    return new Response(
+      "Error: " + (error instanceof Error ? error.message : String(error)),
+      { status: 500 },
+    );
+  }
+});
+
+export default app;
